9c0486199560d58d9eb23f1adad78e5c572b62d9
FabrizioCafolla <developer@fabriziocafolla.com>
2025-05-29 17:17:50 +0200

    update

--- variables.tf
+++ variables.tf
@@ -38,18 +38,6 @@ variable "uptime_check_regions" {
   default     = ["USA_VIRGINIA", "EUROPE", "ASIA_PACIFIC"]
 }
 
-variable "alert_threshold_duration" {
-  type        = string
-  description = "The amount of time that a time series must violate the threshold to be considered failing. Currently, only values that are a multiple of a minute--e.g., 0, 60, 120, or 300 seconds--are supported."
-  default     = "60s"
-}
-
-variable "alert_threshold_value" {
-  type        = number
-  description = "A value against which to compare the time series."
-  default     = 1
-}
-
 variable "uptime_alert_user_labels" {
   type        = map(string)
   description = "This field is intended to be used for labelling the SSL alerts. Labels and values can contain only lowercase letters, numerals, underscores, and dashes. Keys must begin with a letter."
@@ -62,9 +50,28 @@ variable "uptime_monitoring_headers" {
   default     = {}
 }
 
+variable "alert_threshold_duration" {
+  type        = string
+  description = "The amount of time that a time series must violate the threshold to be considered failing. Currently, only values that are a multiple of a minute--e.g., 0, 60, 120, or 300 seconds--are supported."
+  default     = "60s"
+}
+
+variable "alert_threshold_value" {
+  type        = number
+  description = "A value against which to compare the time series."
+  default     = 1
+}
+
 variable "alert_notification_channels" {
   type        = list(string)
   description = "Identifies the notification channels to which notifications should be sent when incidents are opened or closed. The syntax of the entries in this field is projects/[PROJECT_ID]/notificationChannels/[CHANNEL_ID]"
+}
+
+variable "alert_display_name" {
+  type        = string
+  description = "A human-friendly name for the alert policy. Used for monitoring display_name."
+  default     = ""
+  
 }
 
 variable "auth_username" {
